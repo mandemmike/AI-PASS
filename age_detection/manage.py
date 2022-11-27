65e154939350a87319132317d20587e09ba40314
Ediz Genc <gusgened@student.gu.se>
2022-11-27 04:07:54 +0100

    new(toy)-model

--- age_detection/manage.py
+++ age_detection/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "age_detection.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "age_detection_app.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
