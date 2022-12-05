4b8cdff490911c75182cfd3d141a299bd2a2f024
flyf <hry87625@gmail.com>
2022-12-05 07:48:35 +0100

    add new model for MLmodel

--- app/models.py
+++ app/models.py
@@ -9,16 +9,23 @@ class FaceRecognition(models.Model):
         return str(self.record_date)
 
 
+class MLModel(models.Model):
+    model_name = models.CharField("Model's name", max_length=50)
+    model_format = models.CharField("Model's format", max_length=50)
+    model_timestamp = models.DateTimeField(auto_now_add=True)
+    model_file = models.FileField(upload_to='models/')
+
+    def __str__(self) -> str:
+        return str(self.model_timestamp)
+
+
 class TrainedDataset(models.Model):
     # predicted_age = models.FloatField()
     # predicted_gender = models.FloatField()
-    pickle_file = models.FileField()
+    pickle_file = models.FileField(null=True)
+
 
 class TrainingDatasetFile(models.Model):
     file = models.FileField()
     dataset = models.OneToOneField(TrainedDataset, on_delete=models.CASCADE, related_name="dataset_file",
                                    null=True, blank=True)
-
-
-
-
