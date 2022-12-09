f186a6de70e839359e43a9b67a59da94577b3bd8
olgara <olgara@student.chalmers.se>
2022-12-09 21:09:34 +0100

    resolve conflict

--- webservice/main/urls.py
+++ webservice/main/urls.py
@@ -9,6 +9,12 @@ urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     # path('dataset/', views.DatasetListView.as_view(), name='datasets'),
     path('admin_ui/', views.DatasetUploadView.as_view(), name='admin_ui'),
+    path('', views.index, name='index'),
+    # path('AdminUI/', views.AdminUI, name='AdminUI'),
+    path('login/', views.LoginUser, name='login'),
+    path('register/', v2.registerUser, name='register'),
+    path('logout/', views.logoutUser, name='logout')
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
