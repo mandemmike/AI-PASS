91c23be9d811c6ae00ea33f499041cda3f4b9f54
arayam <arayam@student.chalmers.se>
2022-12-10 04:32:08 +0000

    Update views.py

--- webservice/authentication/views.py
+++ webservice/authentication/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
-from rest_framework.permissions import AllowAny
 from .serializers import RegisterSerializer
 from django.contrib.auth import authenticate, login as auth_login
 from django.http import HttpResponse, request
