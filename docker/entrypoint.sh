61883ca80225b9181665368f3e8ad3e9e7cabec2
mandemmike <arayam@student.chalmers.se>
2023-01-13 15:17:04 +0100

    Commit try

--- docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-python manage.py migrate --noinput --run-syncdb 
-python manage.py loaddata fixtures/admin_user.json
-exec "$@"
--- webservice/main/celery.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
-
-app = Celery('main')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
